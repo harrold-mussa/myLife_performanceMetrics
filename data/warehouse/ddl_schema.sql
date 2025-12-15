e645ea24ed48e68d0a2b3cbfdb0ceb21220bcac8
Harrold Mussa <harroldalmussa@gmail.com>
2025-12-15 13:20:36 +0100

    [Fix][1.9] : Quick Schema Fix

--- data/warehouse/ddl_schema.sql
+++ data/warehouse/ddl_schema.sql
@@ -13,58 +13,56 @@ DROP TABLE IF EXISTS Memories_Made;
 
 ---- Fact Table
 
-CREATE TABLE Metrics_table (
-    id SERIAL PRIMARY KEY,
-    goal_metric_id INT,
-    memories_metric_id INT,
-    health_metric_id INT,
-    finance_metric_id INT,
-    happiness_metric_id INT,
-    
-    FOREIGN KEY (goal_metric_id) REFERENCES Goals_Achieved(Goal_metric)
-    FOREIGN KEY (memories_metric_id) REFERENCES Memories_made(Memories_metric),
-    FOREIGN KEY (health_metric_id) REFERENCES Health(Health_metric),
-    FOREIGN KEY (finance_metric_id) REFERENCES Finance(Finance_metric)
-    FOREIGN KEY (happiness_metric_id) REFERENCES Happiness(Happiness_metric)
+CREATE TABLE Metrics_Table (
+    metrics_pk_id SERIAL PRIMARY KEY,
+    Goal_metric_id INTEGER,
+    Memories_metric_id INTEGER,
+    Happiness_metric_id INTEGER,
+    Health_metric_id INTEGER,
+    Finance_metric_id INTEGER,
+
+    FOREIGN KEY (Goal_metric_id) REFERENCES Goals_Achieved(Goal_metric_id),
+    FOREIGN KEY (Memories_metric_id) REFERENCES Memories_Made(Memories_metric_id),
+    FOREIGN KEY (Happiness_metric_id) REFERENCES Happiness(Happiness_metric_id),
+    FOREIGN KEY (Health_metric_id) REFERENCES Health(Health_metric_id),
+    FOREIGN K
 );
 
 ---- Dimensions Tables
 
 CREATE TABLE Health (
-    Health_metric SERIAL PRIMARY KEY,
-    Weight_loss NUMERIC, 
+    Health_metric_id SERIAL PRIMARY KEY,
+    Weight_loss NUMERIC,
     Worked_out BOOLEAN,
-    Took_vitamins BOOLEAN, 
+    Took_vitamins BOOLEAN,
     Ate_healthy BOOLEAN
-)
+);
 
 CREATE TABLE Finance (
-    Finance_metric SERIAL PRIMARY KEY,
-    Updated_budget BOOLEAN, 
+    Finance_metric_id SERIAL PRIMARY KEY,
+    Updated_budget BOOLEAN,
     Saved_money NUMERIC,
     Invested_money NUMERIC
-)
+);
 
 CREATE TABLE Happiness (
-    Happiness_metric SERIAL PRIMARY KEY, 
+    Happiness_metric_id SERIAL PRIMARY KEY,
     Happy_mood BOOLEAN,
     Productive BOOLEAN
-)
+);
 
 CREATE TABLE Goals_Achieved (
-    Goal_metric SERIAL PRIMARY KEY,
+    Goal_metric_id SERIAL PRIMARY KEY, 
     Selfcare_improvements_made BOOLEAN,
-    Financial_improvement_made BOOLEAN,
+    Financial_improvements_made BOOLEAN,
     Health_improvements_made BOOLEAN,
     Read BOOLEAN,
     Studied_OU BOOLEAN,
     Studied_DE BOOLEAN,
     Studied_french BOOLEAN
-)
+);
 
-CREATE TABLE Memories_made (
-    Memories_metric SERIAL PRIMARY KEY,
+CREATE TABLE Memories_Made (
+    Memories_metric_id SERIAL PRIMARY KEY,
     Good_day BOOLEAN
-)
-
-
+);
