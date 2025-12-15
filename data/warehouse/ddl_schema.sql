c5f4f8ff16fb9a025e0d653f86744c2ca2599861
Harrold Mussa <harroldalmussa@gmail.com>
2025-12-15 13:27:05 +0100

    Quick Fix

--- data/warehouse/ddl_schema.sql
+++ data/warehouse/ddl_schema.sql
@@ -1,33 +1,13 @@
--- Data Definitions following the quickdb.txt and star-schema.png image
-
----- Dropping Fact Table if it exists already
+-- DDL for Star Schema based on myLife_performanceMetrics
 
 DROP TABLE IF EXISTS Metrics_Table CASCADE;
-
----- Dropping Dimension Tables
 DROP TABLE IF EXISTS Health;
 DROP TABLE IF EXISTS Finance;
 DROP TABLE IF EXISTS Happiness;
 DROP TABLE IF EXISTS Goals_Achieved;
 DROP TABLE IF EXISTS Memories_Made;
 
----- Fact Table
-
-CREATE TABLE Metrics_Table (
-    metrics_pk_id SERIAL PRIMARY KEY,
-    Goal_metric_id INTEGER,
-    Memories_metric_id INTEGER,
-    Happiness_metric_id INTEGER,
-    Health_metric_id INTEGER,
-    Finance_metric_id INTEGER,
-
-    FOREIGN KEY (Goal_metric_id) REFERENCES Goals_Achieved(Goal_metric_id),
-    FOREIGN KEY (Memories_metric_id) REFERENCES Memories_Made(Memories_metric_id),
-    FOREIGN KEY (Happiness_metric_id) REFERENCES Happiness(Happiness_metric_id),
-    FOREIGN KEY (Health_metric_id) REFERENCES Health(Health_metric_id),
-    FOREIGN KEY (Finance_metric_id) REFERENCES Finance(Finance_metric_id)
-);
----- Dimensions Tables
+-- DIMENSION TABLES
 
 CREATE TABLE Health (
     Health_metric_id SERIAL PRIMARY KEY,
@@ -64,4 +44,21 @@ CREATE TABLE Goals_Achieved (
 CREATE TABLE Memories_Made (
     Memories_metric_id SERIAL PRIMARY KEY,
     Good_day BOOLEAN
+);
+
+-- FACT TABLE
+
+CREATE TABLE Metrics_Table (
+    metrics_pk_id SERIAL PRIMARY KEY,
+    Goal_metric_id INTEGER,
+    Memories_metric_id INTEGER,
+    Happiness_metric_id INTEGER,
+    Health_metric_id INTEGER,
+    Finance_metric_id INTEGER,
+
+    FOREIGN KEY (Goal_metric_id) REFERENCES Goals_Achieved(Goal_metric_id),
+    FOREIGN KEY (Memories_metric_id) REFERENCES Memories_Made(Memories_metric_id),
+    FOREIGN KEY (Happiness_metric_id) REFERENCES Happiness(Happiness_metric_id),
+    FOREIGN KEY (Health_metric_id) REFERENCES Health(Health_metric_id),
+    FOREIGN KEY (Finance_metric_id) REFERENCES Finance(Finance_metric_id)
 );
